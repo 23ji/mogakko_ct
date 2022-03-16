077376e30d72fcf58e109e9fd38139e3da2f8bc6
23ji <85622819+23ji@users.noreply.github.com>
2022-03-16 21:28:16 +0900

    Add files via upload
    Day 7 -  에라토스테네스의 체로 소수 찾기

--- day7.c
+++ day7.c
@@ -1,35 +1,39 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-
-int is_prime(int n) {
-	for (int i = 2; i < n; i++) {
-		if (n % i == 0)
-			return 0;
-	}
-	return 1; 
-}
-
-void print_prime_by_range(int n) {
-	int cnt = 0;
-	for (int num = 2; num < n + 1; num++) {
-		if (is_prime(num))
-			cnt++;
-	}
-	printf("소수의 개수: %d\n", cnt);
-}
-
+#include <math.h>
 
 int main(void)
 {
+	int n = 50000, prime_size = 50001, prime_list[50001];
+	// n의 제곱근 + 1
+	int sqt = sqrt(n) + 1;
 	clock_t start, end;
 	float res;
 
 	start = clock();
-	print_prime_by_range(50000);
-	end = clock();
 
+	// 배열 초기화
+	for (int i = 0; i < prime_size; i++)
+		prime_list[i] = 1;
+
+	// 2부터 제곱근 범위까지, 소수의 배수들을 제외시킴
+	for (int i = 2; i < sqt; i++) {
+		if (prime_list[i]) {
+			for (int j = i + i; j < prime_size; j += i)
+				prime_list[j] = 0;
+		}
+	}
+
+	int cnt = 0;
+
+	for (int i = 2; i < prime_size; i++) {
+		if (prime_list[i]) {
+			cnt++;
+		}
+	}
+
+	end = clock();
+	printf("소수 개수: %d\n", cnt);
 	res = (float)(end - start) / CLOCKS_PER_SEC;
 	printf("실행 시간(초) : %.5f", res);
 }
